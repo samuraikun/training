8f3227f0dfcb6be724e2515490c95deadf098592
Black/Jack <tazitaziawawa@gmail.com>
2015-06-18 01:46:36 +0900

    change
    change

--- js/plan.js
+++ js/plan.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
 	$(".plan").on("click", function(){
-		console.log("clicked");
 		$.get("./js/plan_b.json", function(data){
 			for(var i in data){
-				$("tbody").append("<tr class="+i+"><td></td><td>sddd</td><td>sddd</td><td>sddd</td></tr>")
-				// $('#ID').html(data[i].id);
-				// $('#Name').html(data[i].name);
-				// $('#Rec_Pvs').html(data[i].recordable_pvs_num);
-				// $('#Rec_Recs').html(data[i].recordable_recs_num);
-				// $('#col').html("<td></td>");
+				$("tbody").append("<tr class=" + i + ">" + 
+					"<td>" + data[i].id + "</td>" + 
+					"<td>" + data[i].name + "</td>" + 
+					"<td>" + data[i].recordable_pvs_num + "</td>" + 
+					"<td>" + data[i].recordable_recs_num + "</td>" + 
+					"</tr>");
+				
+				/*$("tbody").append("<td>" + data[i].id + "</td>");
+				$("tbody").append("<td>" + data[i].name + "</td>");
+				$("tbody").append("<td>" + data[i].recordable_pvs_num + "</td>");
+				$("tbody").append("<td>" + data[i].recordable_recs_num + "</td>" + "</tr>");*/
+				
+				/*$('#ID').html(data[i].id);
+				$('#Name').html(data[i].name);
+				$('#Rec_Pvs').html(data[i].recordable_pvs_num);
+				$('#Rec_Recs').html(data[i].recordable_recs_num);
+				$('#col').html("<td></td>");*/
 			};
 		});
 	});

041816a02aed340bdda12363016b8b1f843c8d6f
samuraikun <tazitaziawawa@gmail.com>
2015-06-17 19:43:57 +0900

    cheange
    change

--- js/plan.js
+++ js/plan.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 		console.log("clicked");
 		$.get("./js/plan_b.json", function(data){
 			for(var i in data){
-				$('#ID').html(data[i].id);
-				$('#Name').html(data[i].name);
-				$('#Rec_Pvs').html(data[i].recordable_pvs_num);
-				$('#Rec_Recs').html(data[i].recordable_recs_num);
+				$("tbody").append("<tr class="+i+"><td></td><td>sddd</td><td>sddd</td><td>sddd</td></tr>")
+				// $('#ID').html(data[i].id);
+				// $('#Name').html(data[i].name);
+				// $('#Rec_Pvs').html(data[i].recordable_pvs_num);
+				// $('#Rec_Recs').html(data[i].recordable_recs_num);
+				// $('#col').html("<td></td>");
 			};
 		});
 	});

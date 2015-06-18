93d9a431deb1fe2586bc8aceede041d04b4fd7e4
samuraikun <tazitaziawawa@gmail.com>
2015-06-18 17:22:46 +0900

    618
    618

--- js/plan.js
+++ js/plan.js
@@ -1,14 +1,39 @@
 $(document).ready(function(){
-	$(".plan").on("click", function(){
+	$(".plan").on("click", function(ev){
+		ev.preventDefault();
+		ev.stopPropagation();
 		$.get("./js/plan_b.json", function(data){
-			for(var i in data){
-				$("tbody").append("<tr class=" + i + ">" + 
-					"<td>" + data[i].id + "</td>" + 
-					"<td>" + data[i].name + "</td>" + 
-					"<td>" + data[i].recordable_pvs_num + "</td>" + 
-					"<td>" + data[i].recordable_recs_num + "</td>" + 
-					"</tr>");
-				
+			// Price is 10% discount!!
+			//var arr = [3000, 20000, 100000, 150000, 300000, 100000]; 
+			//JSONから取得して、この配列↑のように格納するには？
+			var arr = data.map(function(e){return e.price});
+			data.map(function(e){return e.price * 1.1}); //10%増しの場合
+			var result = jQuery.map(arr, function(elem, index){return elem - (elem * 0.1)});
+			console.log(result);
+
+			window.data =　data;
+			if (!window.loaded){
+					for(var i in data){
+						$("tbody").append("<tr class=" + i + ">" + 
+						"<td>" + data[i].id + "</td>" + 
+						"<td>" + data[i].name + "</td>" + 
+						"<td>" + data[i].price + "</td>" + 
+						"<td>" + data[i].recordable_recs_num + "</td>" + 
+						"</tr>");
+						window.loaded = true;
+					};
+
+
+	$(".plan-add").on("click", function(){
+		var add_array = new Array();
+		add_array = [$('.ID'), $('.Name'), $('.Rec_Pvs'), $('.Rec_Recs')];
+		$("tbody").append("<tr class=" + 2 + ">" + 
+						"<td>" + add_array[0].val() + "</td>" + 
+						"<td>" + add_array[1].val() + "</td>" +
+						"<td>" + add_array[2].val() + "</td>" +
+						"<td>" + add_array[3].val() + "</td>" + 
+						"</tr>");
+	});
 				/*$("tbody").append("<td>" + data[i].id + "</td>");
 				$("tbody").append("<td>" + data[i].name + "</td>");
 				$("tbody").append("<td>" + data[i].recordable_pvs_num + "</td>");
--- js/post.js
+++ js/post.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-		$(".btn").on("click", function(){
+		$(".btn").on("click", function(ev){
+			ev.preventDefault();
+			ev.stopPropagation();
 			$.get("./js/sample_0617.json", function(data){
 				for(var i in data){
 					$("#output").append("<li>" + data[i].country + " (" + "Your Team is " + data[i].result + ") </li>");
